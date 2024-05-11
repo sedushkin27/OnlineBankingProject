bfa89982c11b92838b9350de3cd5e947eb0a6ef7
sedushkin27 <137596059+sedushkin27@users.noreply.github.com>
2024-05-11 12:22:44 +0300

    Update email_management.py

--- email_management.py
+++ email_management.py
@@ -1,26 +1,18 @@
-import smtplib  # Import the smtplib library to send emails
-from email.message import EmailMessage  # Import the EmailMessage class to create the email message
+import smtplib
+from email.message import EmailMessage
 
-# Set the sender and password for the email account
-sender = ''  # Enter the email address of the sender here
-password = ''  # Enter the password for the email account here
+sender = ''
+password = ''
+
 
 def send_email(recipient, massage):
-    # Create an SMTP object for the Gmail server and specify the port number
     server = smtplib.SMTP("smtp.gmail.com", 587)
-
-    # Enable encryption using the starttls() method
     server.starttls()
 
     try:
-        # Log in to the email account using the sender and password
         server.login(sender, password)
-
-        # Send the email to the recipient with the specified massage
         server.sendmail(sender, recipient, massage)
 
-        # Return a success message
         return 'message sent'
     except Exception as _ex:
-        # Return an error message if there is a problem sending the email
         return f'{_ex} you may not have entered a login and password in email_management.py '
